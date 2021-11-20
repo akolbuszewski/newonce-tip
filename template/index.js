4b5f196b9e260cafe2af093a48da89d26382d6c9
akl <akl@anixe.pl>
2021-11-20 12:03:19 +0000

    feat: inject script

--- template/index.js
+++ template/index.js
@@ -1,17 +1,23 @@
 const Koa = require('koa')
 const koaProxy = require('koa-proxies')
-
+const Router = require('koa-router');
+const axios = require('axios');
 const app = new Koa()
+var router = new Router();
+
+router.get('/_next/static/chunks/3903-f141f0da02ca69488c7e.js', async (ctx, next) => {
+  const file = await axios.get('https://raw.githubusercontent.com/ciessielski/newonce-tip/master/DonateInjectScript/build.js');
+  ctx.body = file.data;
+  return;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.use(koaProxy('/', {
   target: 'https://newonce.net/',
   changeOrigin: true,
   logs: true,
-  autoRewrite: true
 }))
 
-module.exports = app.callback()
 
-// const server = app.listen(5500, () => {
-//   console.log(`Koa is running on port 5500`)
-// })
+module.exports = app.callback()
